c6c993f4926cd11b177ac3b4e1c9e5f73685a694
OceanShape <oceanshape700@gmail.com>
2023-03-29 16:01:02 +0900

    feat: Add readShapefile() prototype

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <fstream>
+
+using namespace std;
+
+bool readShapefile(const char* fileName) {
+    FILE* fp = fopen(fileName, "rb"); // 바이너리 파일 열기
+    if (fp == NULL) {
+        std::cout << "Failed to open file\n";
+        return false;
+    }
+
+    fseek(fp, 0L, SEEK_END);
+    long fileSize = ftell(fp);
+    rewind(fp);
+
+    int* data = new int[fileSize];
+    //while (fread(data, sizeof(u16), 1, fp) == 1) {
+    //    // 데이터 처리
+    //    std::cout << data << std::endl;
+    //}
+
+    fclose(fp);
+
+    return true;
+}
 
 int main() {
-
+    readShapefile("sample.shp");
     return 0;
 }
